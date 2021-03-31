bfd9cce74afc643bf68c4a776f20b8cb57d1f9c8
Konrad <k.brandts@arcor.de>
2021-03-31 14:38:36 +0200

    deleted comments

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 from term import TermParser
 
 #input_string = str(input("Enter units: "))
-input_string = '(1[kg] + 20 [kg]) / 3 [kg]'
+input_string = '(1[kg] + 20 [kg]) / (3 [kg] * 7 [kg])'
 base_term = TermParser(input_string)
 base_term.evaluate()
 
