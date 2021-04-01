0ff15d6d6e1dcf8f128c6b02c39e1d9d167ec321
git-tb <tobibeutlin@gmail.com>
2021-04-01 17:43:59 +0200

    Quasi unverändert

--- src/main.py
+++ src/main.py
@@ -4,10 +4,10 @@ from term import TermParser
 
 #input_string = str(input("Enter units: "))
 input_string = '(2[kg]**4[kg]) / (2 [J^2/s*(N^2/(C))])'
-input_string = '(2^13)/(3^8) * ( (1.097[1/m]*10^7)^3 * 5[m/s]*2[m^2])/(1)'
+input_string = '( ((((2 [J/C]^(2) ))) )/((2[m])^(1)))'
 base_term = TermParser(input_string)
 
 print('============================================')
-print("{:e}".format(base_term.value), str('[' + str(base_term.unit) + ']'))
-print('Better? Unit:' + str(SIConverter.getInstance().SIToUnit(base_term.unit.si_representation,
-                                                            10, 10)))
+print("{:e}".format(base_term.value), '[' + str(base_term.unit) + ']')
+print('Better? Unit: [' + str(SIConverter.getInstance().SIToUnit(base_term.unit.si_representation,
+                                                            10, 10)) + ']')
