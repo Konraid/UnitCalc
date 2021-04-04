bb6a5e34f04f0724cad8d832910f0c25c222bd3e
Konrad <k.brandts@arcor.de>
2021-04-04 21:40:10 +0200

    updated main loop

--- src/main.py
+++ src/main.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
+
+import sys
+
 from SIConverter import SIConverter
 from term import TermParser
 
-#input_string = str(input("Enter units: "))
-input_string = '(2[kg]**4[kg]) / (2 [J^2/s*(N^2/(C))])'
-input_string = '( ((((2 [J/C]^(2) ))) )/((2[m])^(1)))'
-input_string = '[m]+[m]'
-base_term = TermParser(input_string)
+print('######################################')
+print('#              UnitCalc              #')
+print('######################################')
+print('')
 
-print('============================================')
-print("{:e}".format(base_term.value), '[' + str(base_term.unit) + ']')
-print('Better? Unit: [' + str(SIConverter.getInstance().SIToUnit(base_term.unit.si_representation,
-                                                            10, 10)) + ']')
+running = True
+
+while running:
+    input_string = str(input(">> "))
+    if input_string == 'end':
+        running = False
+        break
+    else:
+        base_term = TermParser(input_string)
+        print('============================================')
+        print("{:e}".format(base_term.value), '[' + str(base_term.unit) + ']')
+        print('Better? Unit: [' + str(SIConverter.getInstance().SIToUnit(base_term.unit.si_representation,
+                                                                     10, 10)) + ']')
+
+print('')
+print('ciao...')
