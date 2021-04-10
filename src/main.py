18fdc43a78c6aa0444fec8105afbc6cbb2dc6df0
git-tb <tobibeutlin@gmail.com>
2021-04-10 16:03:30 +0200

    prevented program from crashing after wrong input

--- src/main.py
+++ src/main.py
@@ -24,11 +24,16 @@ while running:
     elif input_string.startswith("$"):
         ConstantsDict.getInstance().AddTempConstant(input_string[1::])
     else:
-        base_term = TermParser(input_string)
-        print('============================================')
-        print("{:e}".format(base_term.value), '[' + str(base_term.unit) + ']')
-        print('Better? Unit: [' + SIConverter.getInstance().SIToUnit(base_term.unit.si_representation,
+        try:
+            base_term = TermParser(input_string)
+            print('============================================')
+            print("{:e}".format(base_term.value), '[' + str(base_term.unit) + ']')
+            print('Better? Unit: [' + SIConverter.getInstance().SIToUnit(base_term.unit.si_representation,
                                                                      10, 10) + ']')
+            print('')
+        except KeyError:
+            print("[ KeyError ] an unknown symbol ocurred (probably), check input")
+            print("")
 
 print('')
 print('ciao...')
