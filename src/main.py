e881a99851e0cef0cda4276ff22e92741b3f29af
Konrad <k.brandts@arcor.de>
2021-04-10 19:25:23 +0200

    improved main loop error handling

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import sys
 from SIConverter import SIConverter
 from term import TermParser
 from ConstantsDict import ConstantsDict
+import traceback
 
 print('######################################')
 print('#              UnitCalc              #')
@@ -17,23 +18,27 @@ print('')
 running = True
 
 while running:
-    input_string = str(input(">> "))
-    if input_string == 'end' or input_string.startswith('exit'):
-        running = False
+    try:
+        input_string = str(input(">> "))
+        if input_string == 'end' or input_string.startswith('exit'):
+            running = False
+            break
+        elif input_string.startswith("$"):
+            ConstantsDict.getInstance().AddTempConstant(input_string[1::])
+        else:
+            try:
+                base_term = TermParser(input_string)
+                print('============================================')
+                print("{:e}".format(base_term.value), '[' + str(base_term.unit) + ']')
+                print('Better? Unit: [' + SIConverter.getInstance().SIToUnit(base_term.unit.si_representation,
+                                                                         10, 10) + ']')
+                print('')
+            except KeyError:
+                print("[ KeyError ] an unknown symbol occurred (probably), check input")
+                traceback.print_exc()
+                print("")
+    except EOFError:
         break
-    elif input_string.startswith("$"):
-        ConstantsDict.getInstance().AddTempConstant(input_string[1::])
-    else:
-        try:
-            base_term = TermParser(input_string)
-            print('============================================')
-            print("{:e}".format(base_term.value), '[' + str(base_term.unit) + ']')
-            print('Better? Unit: [' + SIConverter.getInstance().SIToUnit(base_term.unit.si_representation,
-                                                                     10, 10) + ']')
-            print('')
-        except KeyError:
-            print("[ KeyError ] an unknown symbol occurred (probably), check input")
-            print("")
 
 print('')
 print('ciao...')
